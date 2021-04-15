062d113f1f7b614bcf40b27e29d2eab7d8784e50
AndhikaRei <andhikareihan349@gmail.com>
2021-04-16 02:37:18 +0700

    refactor unused file

--- src/run.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-CALL virt\Scripts\activate
-python Admin_RS_.py
-pause
--- src/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-    # if(len(list_keadaan)<5):
-    #     for i in range(5-len(list_keadaan)):
-    #         list_keadaan.append("Aman")
