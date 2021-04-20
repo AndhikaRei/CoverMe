89ab517e5b385f087621f5c92b5223ea2527be96
gdeananthap <gdeanantha33@gmail.com>
2021-04-20 22:56:05 +0700

    test suhu

--- src/test_suhu.py
+++ src/test_suhu.py
@@ -22,27 +22,6 @@ create_database(cursor,DB_NAME)
 # Membuat Tabel - Apabila tabel sudah ada tidak akan menjadi masalah
 create_table_klien(cursor,DB_NAME)
 
-# # Masukan data kasus_positif, pasien sembuh, pasien meninggal 
-result = get_riwayat_by_id(cursor, DB_NAME, 2)
-if(result[0]==1):
-    print(convert_riwayat_to_listofkeadaan(result[1][0][0]))
-
-result = set_riwayat_by_id(db, cursor, DB_NAME, 2, 27.4)
-print(result[1])
-result = set_riwayat_by_id(db, cursor, DB_NAME, 2, 38)
-print(result[1])
-result = set_riwayat_by_id(db, cursor, DB_NAME, 2, -2)
-print(result[1])
-result = set_riwayat_by_id(db, cursor, DB_NAME, 2, "hehehe")
-print(result[1])
-
-result = get_riwayat_by_id(cursor, DB_NAME, 2)
-if(result[0]==1):
-    print(convert_riwayat_to_listofkeadaan(result[1][0][0]))
-
-result = get_latest_keadaan_by_id(cursor, DB_NAME, 2)
-print(result)
-
 def test_input_suhu_positif_valid():
     """
     Test cek suhu harian klien dengan input yang positif dan valid
